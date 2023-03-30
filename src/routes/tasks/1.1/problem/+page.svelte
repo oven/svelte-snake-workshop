241efe3c8e3c7f47c75d71f4335f50c78278f2f6
Ove Gram Nipen <ove@nipen.no>
2023-03-30 15:22:18 +0200

    ove og sigve

--- src/routes/tasks/1.1/problem/+page.svelte
+++ src/routes/tasks/1.1/problem/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+	import { onMount } from "svelte";
+
 	const TICK_TIME = 100;
 	const BOARD_DIMENSIONS = { x: 20, y: 20 };
 
@@ -7,12 +9,106 @@
 		{ x: 4, y: 3 },
 		{ x: 4, y: 2 },
 	];
-	let apple = { x: 8, y: 8 };
+	let apple = { x: 3, y: 4 };
 	let score = 0;
 
 	const CELL_SIZE = 25;
+
+	function pos(coordinate) {
+		return `top: ${CELL_SIZE * coordinate.y}px; left: ${
+			CELL_SIZE * coordinate.x
+		}px`;
+	}
+
+	let up = { x: 0, y: -1 };
+	let down = { x: 0, y: 1 };
+	let left = { x: -1, y: 0 };
+	let right = { x: 1, y: 0 };
+
+	let currentDirection = down;
+
+	let directions = {
+		37: left,
+		38: up,
+		39: right,
+		40: down,
+	};
+
+	function equals(pos1, pos2) {
+		return pos1.x === pos2.x && pos1.y === pos2.y;
+	}
+
+	function randomPoint() {
+		let x = Math.floor(Math.random() * BOARD_DIMENSIONS.x);
+		let y = Math.floor(Math.random() * BOARD_DIMENSIONS.y);
+		return { x, y };
+	}
+
+	function randomUnoccipiedPoint() {
+		let point = randomPoint();
+
+		while (snake.some((bodyPart) => equals(bodyPart, point))) {
+			point = randomPoint();
+		}
+		return point;
+	}
+
+	function gameLoop() {
+		snake = newSnake(currentDirection);
+	}
+
+	let stopGameLoop = () => {};
+
+	onMount(async () => {
+		const intervalRef = setInterval(gameLoop, 120);
+		stopGameLoop = () => clearInterval(intervalRef);
+		return stopGameLoop;
+	});
+
+	let shouldGrow = false;
+	$: if (equals(snake[0], apple)) {
+		shouldGrow = true;
+		apple = randomUnoccipiedPoint();
+	}
+
+	$: if (isOutsideBoard(snake[0])) {
+		stopGameLoop();
+		alert("you suck");
+	}
+
+	function isOutsideBoard(point) {
+		return (
+			point.x < 0 ||
+			point.x >= BOARD_DIMENSIONS.x ||
+			point.y < 0 ||
+			point.y >= BOARD_DIMENSIONS.y
+		);
+	}
+
+	function newSnake(direction) {
+		let head = snake[0];
+		let newHead = { x: head.x + direction.x, y: head.y + direction.y };
+
+		const foo = [newHead, ...snake];
+		if (!shouldGrow) foo.pop();
+
+		shouldGrow = false;
+		return foo;
+	}
+
+	function onKeydown(event) {
+		let direction = directions[event.keyCode];
+		if (!direction) {
+			return;
+		}
+
+		currentDirection = direction;
+
+		event.preventDefault();
+	}
 </script>
 
+<svelte:body on:keydown={onKeydown} />
 <div class="main-content min-width">
 	<div class="score">{score}</div>
 
@@ -20,9 +116,11 @@
 		class="board"
 		style="--cell-size: {CELL_SIZE}px; --tick-time: {TICK_TIME}ms; --board-size-x: {BOARD_DIMENSIONS.x}; --board-size-y: {BOARD_DIMENSIONS.y}"
 	>
-		<div class="body-part" />
+		{#each snake as bodyPart}
+			<div class="body-part" style={pos(bodyPart)} />
+		{/each}
 
-		<div class="apple" />
+		<div class="apple" style={pos(apple)} />
 	</div>
 </div>
 
